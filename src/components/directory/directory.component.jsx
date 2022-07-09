9e90c51c9d80add4193b5b8cdbb2e046f0c92077
Nisschhal <mrnischalpuri@gmail.com>
2022-07-09 12:15:19 +0545

    setting up different compnent

---
+++ src/components/directory/directory.component.jsx
@@ -0,0 +1,21 @@
+import CategoryItem from "../category-item/category-item.component";
+
+
+import "./directory.style.scss";
+
+const Directory = ({ categories }) => { 
+  return (
+    <div className="directory-container">
+        {/* <CategoryItem key={category.id} category={category} /> */}
+      {/* {categories.map((category) => (
+        ))} */}
+        {categories.map((category) => (
+            <CategoryItem key={category.id}  category={category} />
+     
+       ))} 
+      working
+    </div>
+  );
+};
+
+export default Directory;
